e72d68211c3bc0c4fab28af741f07180f3fb7532
feyhoa <egorbutylo@mail.ru>
2025-03-09 22:04:02 +0300

    feat: finish map class

--- map/app/src/main/java/com/example/map/MapObjects.kt
+++ map/app/src/main/java/com/example/map/MapObjects.kt
@@ -2,10 +2,29 @@ package com.example.map
 import androidx.compose.ui.graphics.Color
 
 
-class Polygon(color: Color = commonWhite) {
+data class Polygon(val id: String, val points: List<Pair<Double, Double>>, var color: Color = commonWhite) {
 
+    fun updateColor(newColor: Color): Unit {
+        color = newColor
+    }
 }
 
-class ColoringMap() {
+class ColoringMap {
+    private val polygons: MutableMap<String, Polygon> = mutableMapOf()
 
+    fun addPolygon(polygon: Polygon): Unit {
+        polygons[polygon.id] = polygon
+    }
+
+    fun removePolygon(id: String) {
+        polygons.remove(id)
+    }
+
+    fun getPolygon(id: String): Polygon? {
+        return polygons[id]
+    }
+
+    fun updatePolygonColor(id: String, color: Color) {
+        polygons[id]?.updateColor(color)
+    }
 }
